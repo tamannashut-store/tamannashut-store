556a1341bdaac51c5a09fe5a4d8e41686db7bb25
sksaifee <sksaifee0204@gmail.com>
2026-05-31 02:41:48 +0530

    p

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,35 +1,17 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
 
-const transporter =
-  nodemailer.createTransport({
+dns.setDefaultResultOrder("ipv4first");
 
-    service: "gmail",
-
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-
-  });
-
-export const sendEmail =
-  async (
-    to,
-    subject,
-    html
-  ) => {
-
-    await transporter.sendMail({
-
-      from:
-        process.env.EMAIL_USER,
-
-      to,
-
-      subject,
-
-      html,
-
-    });
-
-  };
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
