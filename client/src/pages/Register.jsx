6e448f61700bf377b59100c3554aa6156bdabf5b
sksaifee <sksaifee0204@gmail.com>
2026-05-30 11:41:38 +0530

    bug check

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -7,7 +7,7 @@ function Register() {
 
     const navigate = useNavigate();
     const API_URL = import.meta.env.VITE_API_URL;
-
+    console.log("API_URL:", import.meta.env.VITE_API_URL);
     const [formData, setFormData] = useState({
         name: "",
         email: "",
