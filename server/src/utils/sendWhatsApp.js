ef8a86475311b9c3eeea998dc113b8266c1eb9d0
sksaifee <sksaifee0204@gmail.com>
2026-05-31 18:13:54 +0530

    twilio

--- server/src/utils/sendWhatsApp.js
+++ server/src/utils/sendWhatsApp.js
@@ -1,19 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
 import twilio from "twilio";
 
 const client = twilio(
   process.env.TWILIO_SID,
   process.env.TWILIO_AUTH
 );
-
+console.log("SID:", process.env.TWILIO_SID);
+console.log("AUTH:", process.env.TWILIO_AUTH);
 // send whatsapp message
 export const sendWhatsApp = async (to, message) => {
   try {
-    await client.messages.create({
-        from: "whatsapp:+14155238886",
-        to: `whatsapp:${to}`,
-        body: message,
-      });
+    const result = await client.messages.create({
+      from: "whatsapp:+14155238886",
+      to: `whatsapp:${to}`,
+      body: message,
+    });
+
+    console.log("Message sent:", result.sid);
   } catch (err) {
-    console.log("WHATSAPP ERROR:", err.message);
+    console.log("WHATSAPP ERROR:");
+    console.log(err);
   }
 };
