74487bc47865aa0ca94c70445e9fb71fc4bcde6f
sksaifee <sksaifee0204@gmail.com>
2026-05-31 23:46:26 +0530

    email

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,13 +4,15 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    await resend.emails.send({
+    const result = await resend.emails.send({
       from: process.env.EMAIL_FROM,
       to,
       subject,
       html,
     });
+
+    console.log("EMAIL RESULT:", result);
   } catch (error) {
-    console.log("EMAIL ERROR:", error.message);
+    console.log("EMAIL ERROR:", error);
   }
 };
