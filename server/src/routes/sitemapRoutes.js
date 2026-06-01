47cd3ec0d92204b9b2ae8ccd7a1b75f932a33802
sksaifee <sksaifee0204@gmail.com>
2026-06-01 22:28:29 +0530

    Dynamic sitemap

--- server/src/routes/sitemapRoutes.js
+++ server/src/routes/sitemapRoutes.js
@@ -4,10 +4,10 @@ import Product from "../models/Product.js";
 const router = express.Router();
 
 router.get("/sitemap.xml", async (req, res) => {
+    try {
+        const products = await Product.find();
 
-    const products = await Product.find();
-
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>
+        let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 <url>
@@ -19,18 +19,23 @@ router.get("/sitemap.xml", async (req, res) => {
 </url>
 `;
 
-    products.forEach((product) => {
-        xml += `
+        products.forEach((product) => {
+            xml += `
 <url>
 <loc>https://tamannashut.com/product/${product._id}</loc>
 </url>
 `;
-    });
+        });
 
-    xml += `</urlset>`;
+        xml += `</urlset>`;
 
-    res.header("Content-Type", "application/xml");
-    res.send(xml);
+        res.header("Content-Type", "application/xml");
+        res.send(xml);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Sitemap Error");
+    }
 });
 
 export default router;
