3b343990e862f82e889096630d8a205312416929
sksaifee <sksaifee0204@gmail.com>
2026-06-04 00:00:24 +0530

    g

--- server/src/routes/googleFeedRoutes.js
+++ server/src/routes/googleFeedRoutes.js
@@ -11,36 +11,42 @@ router.get("/google-feed.xml", async (req, res) => {
 <rss version="2.0"
 xmlns:g="http://base.google.com/ns/1.0">
 <channel>
-
-<title>Tamanna's Hut</title>
+<title><![CDATA[Tamanna's Hut]]></title>
 <link>https://tamannashut.com</link>
-<description>Kids Fashion Store</description>`;
+<description><![CDATA[Kids Fashion Store]]></description>`;
 
     products.forEach((product) => {
+
+      const imageUrl = product.image?.startsWith("http")
+        ? product.image
+        : `https://tamannashut-store.onrender.com${product.image}`;
+
       xml += `
 <item>
-  <g:id>${product._id}</g:id>
-  <g:title><![CDATA[${product.name}]]></g:title>
-  <g:description><![CDATA[${product.description || product.name}]]></g:description>
+<g:id>${product._id}</g:id>
 
-  <g:link>
-    https://tamannashut.com/product/${product._id}
-  </g:link>
+<g:title><![CDATA[${product.name || ""}]]></g:title>
 
-  <g:image_link>
-    ${product.image}
-  </g:image_link>
+<g:description><![CDATA[
+${product.description || product.name || ""}
+]]></g:description>
 
-  <g:availability>in_stock</g:availability>
-  <g:condition>new</g:condition>
+<g:link>https://tamannashut.com/product/${product._id}</g:link>
 
-  <g:price>${product.price} INR</g:price>
+<g:image_link>${imageUrl}</g:image_link>
 
-  <g:brand>Tamanna's Hut</g:brand>
+<g:availability>in stock</g:availability>
 
-  <g:google_product_category>
-    Apparel & Accessories > Clothing
-  </g:google_product_category>
+<g:condition>new</g:condition>
+
+<g:price>${product.price} INR</g:price>
+
+<g:brand><![CDATA[Tamanna's Hut]]></g:brand>
+
+<g:google_product_category>
+Apparel & Accessories > Clothing
+</g:google_product_category>
+
 </item>`;
     });
 
@@ -48,7 +54,7 @@ xmlns:g="http://base.google.com/ns/1.0">
 </channel>
 </rss>`;
 
-    res.header("Content-Type", "application/xml");
+    res.set("Content-Type", "application/xml");
     res.send(xml);
 
   } catch (error) {
