b87a54ece15566d4615782e6da6f7c0bb94c9079
sksaifee <sksaifee0204@gmail.com>
2026-06-03 23:36:46 +0530

    feed update

--- server/src/routes/googleFeedRoutes.js
+++ server/src/routes/googleFeedRoutes.js
@@ -4,10 +4,10 @@ import Product from "../models/Product.js";
 const router = express.Router();
 
 router.get("/google-feed.xml", async (req, res) => {
+  try {
+    const products = await Product.find();
 
-  const products = await Product.find();
-
-  let xml = `<?xml version="1.0"?>
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
 xmlns:g="http://base.google.com/ns/1.0">
 <channel>
@@ -16,25 +16,45 @@ xmlns:g="http://base.google.com/ns/1.0">
 <link>https://tamannashut.com</link>
 <description>Kids Fashion Store</description>`;
 
-  products.forEach((product) => {
+    products.forEach((product) => {
+      xml += `
+<item>
+  <g:id>${product._id}</g:id>
+  <g:title><![CDATA[${product.name}]]></g:title>
+  <g:description><![CDATA[${product.description || product.name}]]></g:description>
+
+  <g:link>
+    https://tamannashut.com/product/${product._id}
+  </g:link>
+
+  <g:image_link>
+    ${product.image}
+  </g:image_link>
+
+  <g:availability>in_stock</g:availability>
+  <g:condition>new</g:condition>
+
+  <g:price>${product.price} INR</g:price>
+
+  <g:brand>Tamanna's Hut</g:brand>
+
+  <g:google_product_category>
+    Apparel & Accessories > Clothing
+  </g:google_product_category>
+</item>`;
+    });
 
     xml += `
-<item>
-<g:id>${product._id}</g:id>
-<title>${product.name}</title>
-<link>https://tamannashut.com/product/${product._id}</link>
-<g:price>${product.price} INR</g:price>
-<g:condition>new</g:condition>
-<g:availability>in stock</g:availability>
-</item>`;
-  });
-
-  xml += `
 </channel>
 </rss>`;
 
-  res.header("Content-Type", "application/xml");
-  res.send(xml);
+    res.header("Content-Type", "application/xml");
+    res.send(xml);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Feed Error");
+  }
 });
 
 export default router;
