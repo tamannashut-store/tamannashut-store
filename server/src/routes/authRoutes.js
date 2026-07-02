02440dfeb6535bf97f8c98fd7e098eae901a23ec
sksaifee <sksaifee0204@gmail.com>
2026-07-02 23:35:41 +0530

    G

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
